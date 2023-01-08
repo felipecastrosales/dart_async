ed0d8ff49bf77ece007eb942358201b5745b2266
Felipe Sales <soufeliposales@gmail.com>
2023-01-08 20:20:18 -0300

    chore: future anatomy

---
+++ lib/futures/c_futures.dart
@@ -0,0 +1,28 @@
+void main() {
+  // Future -> Status
+  //   Incomplete.
+  //   Complete with success.
+  //   Complete with error.
+
+  Future(() {
+    // async function
+    print('Future(() { ... })');
+  });
+
+  var value = Future(() {}).then(
+    (value) {
+      // register callback - executed when future is completed and with success
+      print('Future(() { ... }).then((value) => print(value))');
+    },
+  ).catchError(
+    (error) {
+      // register callback - executed when future is completed and with error
+      print('Future(() { ... }).catchError((error) => print(error))');
+    },
+  ).whenComplete(
+    () {
+      // register callback - always executed (when success and/or error)
+      print('Future(() { ... }).whenComplete(() => print(completed))');
+    },
+  );
+}
